d5e4336fd552d9cdbbad2b0a3997e9d3fa8a5f52
Jeremiah Thomas <jeremiahjthomas21@gmail.com>
2023-07-16 22:25:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+
+/**CODE FOR BOTTOM RIGHT BUTTONS **/
+
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        $('.btn_controls').show();
+      } else {
+        $('.btn_controls').hide();
+      }
+    });
+
+    scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+/**TODO: ADD FUNCTION TO MAKE SIDEBAR DISAPPEAR UNTIL SCREEN REACHES 50% OF PAGE */
